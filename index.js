715f126519f8f652788bbd0c2e27777b4bcfd972
Alexander kagerer <kagerer19@gmail.com>
2020-05-01 15:27:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function makeInt(n, base) {
    return makeInt;
 }
 function preserveDecimal(n, base) {
-  var preserveDecimal = parseFloat('FF2', 2.222);
+  var preserveDecimal = 
   return preserveDecimal;
 }
 
